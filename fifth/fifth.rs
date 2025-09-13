8c651886755b585ca9dac359dda91b023a07a0e7
seunggyu-labtop <sgkevinlee@naver.com>
2025-09-13 00:30:15 -0400

    feat: implemented fifth

---
+++ fifth/fifth.rs
@@ -0,0 +1,99 @@
+use std::env;
+use std::fs;
+use std::collections::{HashMap, HashSet};
+
+// #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+// struct Node {
+//     name: String,
+//     weight: i32,
+// }
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let graph_file = &args[1];
+    let query_file = &args[2];
+
+    let contents = fs::read_to_string(graph_file).expect("Cannot read file");
+    let mut lines = contents.lines();
+    let node_num: usize = lines.next().unwrap().parse().unwrap();
+
+    let mut nodes: Vec<_> = lines.by_ref().take(node_num).collect();
+    // println!("{:?}", nodes);
+    // println!("{:?}", lines.next().unwrap());
+    let mut graph: HashMap<String, Vec<(String, i32)>> = HashMap::new();
+    for node in nodes {
+        graph.insert(node.to_string().clone(), Vec::new());
+    }
+    for line in lines {
+        let tmp: Vec<_> = line.split_whitespace().collect();
+        graph.get_mut(&tmp[0].to_string()).unwrap().push((tmp[1].to_string(), tmp[2].parse().unwrap()));
+        // graph[tmp[0]].push((tmp[1].to_string(), tmp[2].parse().unwrap()));
+    }
+    // println!("{:?}", graph);
+    
+    let query = fs::read_to_string(query_file).expect("Cannot read file");
+    for source in query.lines() {
+        let source = source.to_string();
+        // for node in graph.values() {
+        //     println!("{:?}", topological_sort(node));
+        // }
+        // let topo_node = topological_sort(&graph);
+        // let mut traversed = HashSet::new();
+        let mut node_order: Vec<String> = Vec::new();
+        // println!("{:?}", graph[source]);
+        // println!("{:?}", print_type(&graph[source]));
+        // for node in graph.keys() {
+        //     dfs(&node, &graph, &mut node_order);
+        // }
+        dfs(&source, &graph, &mut node_order);
+        for node in graph.keys() {
+            if !node_order.contains(&node) {
+                dfs(&node, &graph, &mut node_order);
+            }
+        }
+        let path = shortest_path_dag(&graph, &mut node_order, &source);
+        // for (node, dist) in path {
+        //     println!("{} {}", node, if dist == i32::MAX { "INF" } else { &dist.to_string() });
+        // }
+        let mut sorted_node: Vec<_> = path.keys().map(|s| s.to_string()).collect();
+        sorted_node.sort();
+        for node in sorted_node {
+            println!("{} {}", node, if path[&node] == i32::MAX { "INF" } else { &path[&node].to_string() });
+        }
+        println!();
+    }
+}
+fn topological_sort(adj: &Vec<(String, i32)>) -> Vec<(String, i32)> {
+    let mut tmp = adj.clone();
+    tmp.sort_by(|a, b| a.0.cmp(&b.0));
+    tmp
+}
+// fn dfs(source: &String, graph: &HashMap<String, Vec<(String, i32)>>, traversed: &mut HashSet<String>, order: &mut Vec<String>) {
+fn dfs(source: &String, graph: &HashMap<String, Vec<(String, i32)>>, order: &mut Vec<String>) {
+    // traversed.insert(source.clone());
+    order.push(source.clone());
+    for node in topological_sort(&graph[source]) {
+        // println!("{:?}", node.0);
+        let nxt = node.0.clone();
+        if !order.contains(&nxt) {
+            dfs(&nxt, graph, order);
+        }
+    }
+}
+fn shortest_path_dag(graph: &HashMap<String, Vec<(String, i32)>>, order: &mut Vec<String>, source: &String) -> HashMap<String, i32> {
+    let mut dist: HashMap<String, i32> = graph.keys().map(|k| (k.clone(), i32::MAX)).collect();
+
+    dist.insert(source.clone(), 0);
+    for node in order {
+        let cur_dist = *dist.get(node).unwrap();
+        if cur_dist != i32::MAX {
+            for (k, v) in &graph[node] {
+                let new_dist = cur_dist + v;
+                if new_dist < *dist.get(k).unwrap() {
+                    dist.insert(k.clone(), new_dist);
+                }
+            }
+        }
+    }
+    dist
+}
