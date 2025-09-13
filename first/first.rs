4d8039f392af0daa841038db02d09f46d4188bd6
seunggyu-labtop <sgkevinlee@naver.com>
2025-09-13 01:56:36 -0400

    refactor: first

--- first/first.rs
+++ first/first.rs
@@ -1,76 +1,51 @@
 use std::env;
 use std::fs;
-use std::io;
+use std::collections::HashMap;
 
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct Node {
-    name: String,
-}
-
-fn main() -> io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().collect();
     let graph_file = &args[1];
     let query_file = &args[2];
 
-    let contents = fs::read_to_string(graph_file)?;
-    let line: Vec<_> = contents.split("\n").collect();
-    let mut nodes: Vec<Vec<Node>> = vec![];
-    let node_num: usize = line[0].parse::<usize>().unwrap();
-    // println!("Node num: {}", node_num);
-    // let nodes_vec = &line[1..=node_num];
-    for node in &line[1..=node_num] {
-        let tmp_node = Node { name: node.to_string() };
-        nodes.push(vec![tmp_node]);
+    let contents = fs::read_to_string(graph_file).expect("Cannot read file");
+    let mut lines = contents.lines();
+    let node_num: usize = lines.next().unwrap().parse().unwrap();
+    let mut nodes: Vec<_> = lines.by_ref().take(node_num).collect();
+    let mut graph: HashMap<String, Vec<String>> = HashMap::new();
+    for node in nodes {
+        graph.insert(node.to_string().clone(), Vec::new());
     }
-    // println!("Nodes: {:?}", nodes);
-    // let vertices = &line[node_num+1..];
-    for vertex in &line[node_num+1..] {
-        let tmp_nodes: Vec<String> = vertex.split_whitespace().map(|s| s.to_string()).collect();
-        let (a, b) = (&tmp_nodes[0], &tmp_nodes[1]);
-        for node in &mut nodes {
-            if node[0].name == *a {
-                node.push(Node { name: b.clone()});
-            }
-            else if node[0].name == *b {
-                node.push(Node { name: a.clone()});
-                break;
-            }
-        }
+    
+    for line in lines {
+        let tmp: Vec<_> = line.split_whitespace().collect();
+        graph.get_mut(&tmp[0].to_string()).unwrap().push(tmp[1].to_string());
+        graph.get_mut(&tmp[1].to_string()).unwrap().push(tmp[0].to_string());
     }
-    // println!("Vertices: {:?}", vertices);
-    // println!("Connected nodes: ");
-    // for node in nodes {
-    //     println!("{:?}", node);
-    // }
-    // println!("connected nodes: {:?}", nodes);
 
-    let contents = fs::read_to_string(query_file)?;
-    let queries: Vec<_> = contents.split("\n").collect();
-    for query in queries {
+    let queries = fs::read_to_string(query_file).expect("Cannot read file");
+    for query in queries.lines() {
         let tmp_query: Vec<String> = query.split_whitespace().map(|s| s.to_string()).collect();
         let (a, b) = (&tmp_query[0], &tmp_query[1]);
         if a == "d" {
-            for node in &nodes {
-                if node[0].name == *b {
-                    println!("{}", node.len() - 1);
+            for node in graph.keys() {
+                if node == b {
+                    println!("{}", graph[node].len());
+                    break;
                 }
             }
         }
         else if a == "a" {
-            for node in &mut nodes {
-                if node[0].name == *b {
-                    let adj: &mut [Node] = &mut node[1..];
-                    adj.sort();
-                    // println!("{:?}", adj);
-                    for n in adj {
-                        print!("{} ", n.name);
+            for (node, adj) in &graph {
+                if *node == *b {
+                    let mut tmp = adj.clone();
+                    tmp.sort_by(|a, b| a.cmp(&b));
+                    for n in tmp {
+                        print!("{} ", n);
                     }
                     println!();
+                    break;
                 }
             }
         }
     }
-
-
-    Ok(())
 }
