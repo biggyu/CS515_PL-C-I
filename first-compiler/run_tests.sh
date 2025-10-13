e245bcfd3d32445066ada14251fbfd211bc4db8d
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2025-10-13 02:13:22 -0400

    feat: test1

--- first-compiler/run_tests.sh
+++ first-compiler/run_tests.sh
@@ -3,9 +3,12 @@
 # Name of your compiled parser
 EXECUTABLE="./first-compiler"
 
-for input_file in test*.input; do
-    output_file="${input_file%.input}.output"
-    $EXECUTABLE "$input_file" > "$output_file"
+for input_file in test*.exp; do
+    output_file1="${input_file%.exp}.astdag"
+    output_file2="${input_file%.exp}.ll"
+    $EXECUTABLE "$input_file" > "$output_file1"
+    mv first.ll $output_file2
 done
 
 echo " All tests completed."
+
