2616511782bae5bcfb127f0d0edc62044424b0c9
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2025-10-31 12:27:26 -0400

    fix: split commas

--- ru-compiler/trivial-ssa/src/scanner.rs
+++ ru-compiler/trivial-ssa/src/scanner.rs
@@ -18,8 +18,10 @@ pub fn tokenize(rhs: &str) -> Vec<String> {
 
 pub fn separate_chars(s: &str) -> Vec<String> {
     let mut sep = separate_brackets(&s);
-    sep = separate_semicolons(&s);
+    sep = separate_semicolons(&sep);
+    sep = separate_commas(&sep);
     sep.split_whitespace().map(|s| s.to_string()).collect()
+    
 }
 
 pub fn separate_brackets(s: &str) -> String {
@@ -46,6 +48,21 @@ pub fn separate_semicolons(s: &str) -> String {
 
     for ch in s.chars() {
         if ch == ';'{
+            tokens.push(' ');
+            tokens.push(ch);
+        }
+        else {
+            tokens.push(ch);
+        }
+    }
+    tokens
+}
+
+pub fn separate_commas(s: &str) -> String {
+    let mut tokens = String::new();
+
+    for ch in s.chars() {
+        if ch == ','{
             tokens.push(' ');
             tokens.push(ch);
         }
