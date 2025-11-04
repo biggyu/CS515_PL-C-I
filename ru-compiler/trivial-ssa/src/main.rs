411ec1b265ddd7d6c9995575325c39be139af45c
Seung Gyu Lee <sl2486@ilab1.cs.rutgers.edu>
2025-11-04 00:12:23 -0500

    feat: llvm_ir for new rules

--- ru-compiler/trivial-ssa/src/main.rs
+++ ru-compiler/trivial-ssa/src/main.rs
@@ -26,15 +26,13 @@ fn main() -> io::Result<()> {
     let contents = fs::read_to_string(input_file).expect("Cannot read file");
     let lines = contents.lines();
 
-    let mut expressions: Vec<Vec<_>> = Vec::new();
+    let mut expressions: Vec<_> = Vec::new();
+    // let mut expressions: Vec<Vec<_>> = Vec::new();
     for line in lines {
-        let separated = separate_brackets(&line);
-        let tokens: Vec<String> = separated.split_whitespace().map(|s| s.to_string()).collect();
-
-        expressions.push(tokens);
+        expressions.extend(separate_chars(&line));
     }
-    // for expr in expressions {
-    //     println!("{:?}", expr);
+    // for expr in &expressions {
+    //     println!("{}", expr);
     // }
 
     //Context Free Grammar
@@ -66,71 +64,138 @@ fn main() -> io::Result<()> {
 
     let mut parse_table: HashMap<(String, String), ProductionRule> = HashMap::new();
     parse_table = gen_parse_table(&llrules, &firsts, &follows);
+    // for key in parse_table.keys() {
+    //     println!("{:?} {:?}", key, parse_table[key]);
+    // }
 
-    for expression in expressions {
-        // parse tree
-        let mut tmp = expression.iter().map(|s| s.to_string()).collect();
-        let parsed = ll1_parse(&mut tmp, &parse_table, &start_symbol);
-        //TODO: print format
-        if let Ok(root) = parsed {
-            let mut parse_traversed = HashMap::new();
-            bfs_parse(&root, 0, &mut parse_traversed);
-            let mut ordered_keys: Vec<_> = parse_traversed.keys().into_iter().collect();
+    let parsed = ll1_parse(&mut expressions, &parse_table, &start_symbol);
+    if let Ok(root) = parsed {
+        // let mut ordered_keys = Vec::new();
+        // println!("{:?}", root);
+        // let mut parse_traversed = HashMap::new();
+        // bfs_parse(&root, 0, &mut parse_traversed);
+        // ordered_keys = parse_traversed.keys().into_iter().collect();
+        // ordered_keys.sort_by(|x, y| x.cmp(&y));
+        // for key in ordered_keys {
+        //     for term in &parse_traversed[key] {
+        //         print!("{} ", term);
+        //     }
+        //     println!();
+        // }
+
+        let ast = gen_ast(&root);
+        if let Ok(root) = ast {
+            // AST
+            // let mut ast_traversed = HashMap::new();
+            // bfs_ats(&root, 0, &mut ast_traversed);
+            // ordered_keys = ast_traversed.keys().into_iter().collect();
             // ordered_keys.sort_by(|x, y| x.cmp(&y));
+            // println!("AST");
             // for key in ordered_keys {
-            //     for term in &parse_traversed[key] {
+            //     for term in &ast_traversed[key] {
+            //         print!("{} ", term);
+            //     }
+            //     println!();
+            // }
+            // println!("{:?}", root);
+            // Directed Acyclic Representation
+            let mut value_nums: HashMap<DAGNode, usize> = HashMap::new();
+            let mut cur_valnum: usize = 0;
+            let mut dag_nodes: HashMap<usize, Rc<DAGNode>> = HashMap::new();
+            let (_, dag) = dag_rep(&root, &mut value_nums, &mut cur_valnum, &mut dag_nodes);
+            
+            // println!("{:?}", dag);
+            // let mut dag_traversed = HashMap::new();
+            // bfs_dag(&*dag.clone(), 0, &mut dag_traversed);
+            // ordered_keys = dag_traversed.keys().into_iter().collect();
+            // ordered_keys.sort_by(|x, y| x.cmp(&y));
+            // println!("DAG");
+            // for key in ordered_keys {
+            //     for term in &dag_traversed[key] {
             //         print!("{} ", term);
             //     }
             //     println!();
             // }
 
-            let ast = gen_ast(&root);
-            if let Ok(root) = ast {
-                // AST
-                let mut ast_traversed = HashMap::new();
-                bfs_ats(&root, 0, &mut ast_traversed);
-                ordered_keys = ast_traversed.keys().into_iter().collect();
-                ordered_keys.sort_by(|x, y| x.cmp(&y));
-                println!("AST");
-                for key in ordered_keys {
-                    for term in &ast_traversed[key] {
-                        print!("{} ", term);
-                    }
-                    println!();
-                }
-                // Directed Acyclic Representation
-                let mut value_nums: HashMap<DAGNode, usize> = HashMap::new();
-                let mut cur_valnum: usize = 0;
-                let mut dag_nodes: HashMap<usize, Rc<DAGNode>> = HashMap::new();
-                let (_, dag) = dag_rep(&root, &mut value_nums, &mut cur_valnum, &mut dag_nodes);
-
-                let mut dag_traversed = HashMap::new();
-                bfs_dag(&*dag.clone(), 0, &mut dag_traversed);
-                ordered_keys = dag_traversed.keys().into_iter().collect();
-                ordered_keys.sort_by(|x, y| x.cmp(&y));
-                println!("DAG");
-                for key in ordered_keys {
-                    for term in &dag_traversed[key] {
-                        print!("{} ", term);
-                    }
-                    println!();
-                }
-
-                //LLVM IR
-                let mut temp_nums: HashMap<DAGNode, usize> = HashMap::new();
-                let llvm_ir = gen_llvm_ir(&*dag.clone(), &mut temp_nums, &dag_nodes, Some("i64".to_string()), Some("foo".to_string()));
-                let output_file: Vec<_> = input_file.split(".").collect();
-                fs::write(format!("{}.ll", output_file[0]), &llvm_ir)?;
-                // println!("{}\nfirst.ll created successfully", llvm_ir);
-            }
-            else {
-                println!("{:?}", ast);
-            }
+            //LLVM IR
+            let mut temp_nums: HashMap<DAGNode, usize> = HashMap::new();
+            let llvm_ir = gen_llvm_ir(&*dag.clone(), &mut temp_nums, &dag_nodes, Some("i64".to_string()), Some("foo".to_string()));
+            let output_file: Vec<_> = input_file.split(".").collect();
+            fs::write(format!("{}.ll", output_file[0]), &llvm_ir)?;
+            // // println!("{}\nfirst.ll created successfully", llvm_ir);
         }
         else {
-            println!("{:?}", parsed);
+            println!("{:?}", ast);
         }
-        println!();
     }
+    else {
+        println!("{:?}", parsed);
+    }
+
+    // for expression in expressions {
+    //     // parse tree
+    //     let mut tmp = expression.iter().map(|s| s.to_string()).collect();
+    //     let parsed = ll1_parse(&mut tmp, &parse_table, &start_symbol);
+    //     //TODO: print format
+    //     if let Ok(root) = parsed {
+    //         let mut parse_traversed = HashMap::new();
+    //         bfs_parse(&root, 0, &mut parse_traversed);
+    //         let mut ordered_keys: Vec<_> = parse_traversed.keys().into_iter().collect();
+    //         // ordered_keys.sort_by(|x, y| x.cmp(&y));
+    //         // for key in ordered_keys {
+    //         //     for term in &parse_traversed[key] {
+    //         //         print!("{} ", term);
+    //         //     }
+    //         //     println!();
+    //         // }
+
+    //         let ast = gen_ast(&root);
+    //         if let Ok(root) = ast {
+    //             // AST
+    //             let mut ast_traversed = HashMap::new();
+    //             bfs_ats(&root, 0, &mut ast_traversed);
+    //             ordered_keys = ast_traversed.keys().into_iter().collect();
+    //             ordered_keys.sort_by(|x, y| x.cmp(&y));
+    //             println!("AST");
+    //             for key in ordered_keys {
+    //                 for term in &ast_traversed[key] {
+    //                     print!("{} ", term);
+    //                 }
+    //                 println!();
+    //             }
+    //             // Directed Acyclic Representation
+    //             let mut value_nums: HashMap<DAGNode, usize> = HashMap::new();
+    //             let mut cur_valnum: usize = 0;
+    //             let mut dag_nodes: HashMap<usize, Rc<DAGNode>> = HashMap::new();
+    //             let (_, dag) = dag_rep(&root, &mut value_nums, &mut cur_valnum, &mut dag_nodes);
+
+    //             let mut dag_traversed = HashMap::new();
+    //             bfs_dag(&*dag.clone(), 0, &mut dag_traversed);
+    //             ordered_keys = dag_traversed.keys().into_iter().collect();
+    //             ordered_keys.sort_by(|x, y| x.cmp(&y));
+    //             println!("DAG");
+    //             for key in ordered_keys {
+    //                 for term in &dag_traversed[key] {
+    //                     print!("{} ", term);
+    //                 }
+    //                 println!();
+    //             }
+
+    //             //LLVM IR
+    //             let mut temp_nums: HashMap<DAGNode, usize> = HashMap::new();
+    //             let llvm_ir = gen_llvm_ir(&*dag.clone(), &mut temp_nums, &dag_nodes, Some("i64".to_string()), Some("foo".to_string()));
+    //             let output_file: Vec<_> = input_file.split(".").collect();
+    //             fs::write(format!("{}.ll", output_file[0]), &llvm_ir)?;
+    //             // println!("{}\nfirst.ll created successfully", llvm_ir);
+    //         }
+    //         else {
+    //             println!("{:?}", ast);
+    //         }
+    //     }
+    //     else {
+    //         println!("{:?}", parsed);
+    //     }
+    //     println!();
+    // }
     Ok(())
 }
